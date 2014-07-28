389f069e7ab6839601f6db3f5669e405d207a7af
Ben Paddock <pads@thisispads.me.uk>
2014-07-28 15:04:07 +0100

    Include pagespeed as part of the build
    Also use 2 spaces for JS indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,41 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        jshint: {
-            options: {
-                jshintrc: ".jshintrc"
-            },
-            gruntfile: {
-                src: "Gruntfile.js"
-            }
-        },
-        exec: {
-            twserve: {
-                command: "tsapp serve",
-                stdout: true
-            },
-            twpush: {
-                command: "tsapp push_hard public",
-                stdout: true
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    jshint: {
+      options: {
+        jshintrc: ".jshintrc"
+      },
+      gruntfile: {
+        src: "Gruntfile.js"
+      }
+    },
+    exec: {
+      twserve: {
+        command: "tsapp serve",
+        stdout: true
+      },
+      twpush: {
+        command: "tsapp push_hard public",
+        stdout: true
+      }
+    },
+    pagespeed: {
+      production: {
+        options: {
+          nokey: true,
+          url: "http://benpaddock.net",
+//          paths: [],
+          locale: "en_GB",
+          strategy: "desktop",
+          threshold: 90
         }
-    });
+      }
+    }
+  });
+  /*global require:false*/
+  require("matchdep").filterDev("grunt-!(template)*").forEach(grunt.loadNpmTasks);
 
-    grunt.loadNpmTasks("grunt-exec");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-
-    grunt.registerTask("default", ["jshint"]);
+  grunt.registerTask("ci", ["jshint", "pagespeed"]);
 };
