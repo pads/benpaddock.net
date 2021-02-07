b25f7cf72e46dc67ff3422c3737561998da40200
Ben Paddock <pads@thisispads.me.uk>
2021-02-07 15:44:31 +0000

    Add unused imports plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,8 +18,11 @@ module.exports = {
         "prettier/@typescript-eslint",
         "plugin:prettier/recommended",
     ],
+    plugins: ["unused-imports"],
     rules: {
         "react/react-in-jsx-scope": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports-ts": "error",
     },
     globals: {
         React: "writable",
