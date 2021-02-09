453c58c31585fa9584788a33756dde86dd388ddb
Ben Paddock <pads@thisispads.me.uk>
2021-02-09 11:07:23 +0000

    Add custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        fontFamily: {
+            sans: ["Raleway", "Open Sans", "sans-serif"],
+            serif: ["'Roboto Slab'", "Open Sans", "serif"],
+        },
     },
     variants: {
         extend: {},
