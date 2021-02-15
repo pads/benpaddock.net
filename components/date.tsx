af5418150880ff22c8dac78b6b4b4f9accc8c648
Ben Paddock <pads@thisispads.me.uk>
2021-02-15 15:39:47 +0000

    Add icon to date

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,13 @@
+import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { format, parseISO } from "date-fns";
 
 export default function Date({ dateString }: { dateString: string }): JSX.Element {
     const date = parseISO(dateString);
-    return <time dateTime={dateString}>{format(date, "do LLLL, yyyy")}</time>;
+    return (
+        <span>
+            <FontAwesomeIcon className="mr-2" icon={faCalendarAlt} fixedWidth size="sm" />
+            <time dateTime={dateString}>{format(date, "do LLLL, yyyy")}</time>
+        </span>
+    );
 }
